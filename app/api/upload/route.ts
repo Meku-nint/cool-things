103adb6fd5d9073b803ade9745aa64106a537d5a
davetes <tesfa4706@gmail.com>
2025-10-31 09:16:09 +0300

    change route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs/promises';
 import path from 'path';
-import prisma from '../../../../lib/prisma';
+import prisma from '../../../lib/prisma';
 
 export const runtime = 'node';
 
